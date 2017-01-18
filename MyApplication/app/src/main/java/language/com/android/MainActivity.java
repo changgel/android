182596f991641f6d7ec66f64fdba869da4099a38
yangshuyu <810043299@qq.com>
2017-01-18 16:37:26 +0800

    改变之后上传到分支

--- MyApplication/app/src/main/java/language/com/android/MainActivity.java
+++ MyApplication/app/src/main/java/language/com/android/MainActivity.java
@@ -2,12 +2,27 @@ package language.com.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv_main;
+    private Button btn_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_main = (TextView)findViewById(R.id.tv_main);
+        btn_main = (Button)findViewById(R.id.btn_main);
+
+        btn_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_main.setText("这是分支");
+            }
+        });
     }
 }
